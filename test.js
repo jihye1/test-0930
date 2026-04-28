cf30b756b95ec9881fdc52c6546d8c8daf26d426
jihye1 <jihye.won1@samsung.com>
2026-04-28 13:40:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -18,14 +18,3 @@ console.log(myArray[2]); // expected value was 'd' but output is undefined
 function multiply(a = 1, b) {  // Noncompliant
   return a*b;
 }
-
-var x = multiply(42);  // returns NaN as b is undefined
-
-
-function MyClass(name, strings) {
-  name = foo;                    // Noncompliant
-
-  for (var str of strings) {
-    str = "";  // Noncompliant
-  }
-}
