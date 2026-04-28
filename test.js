79dbc696987c0953d8d0406ac0ba9b7266c49f03
jihye1 <jihye.won1@samsung.com>
2026-04-28 14:12:00 +0900

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,3 @@ class Dog extends Animal {
 }
 
 var myArray = ['a', 'b', 'c', 'd'];
-
-delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
-console.log(myArray[2]); // expected value was 'd' but output is undefined
